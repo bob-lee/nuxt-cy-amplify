5f627a0141715a96f7206e9457073f5281b9d2aa
bob-lee <bob.bumsuk.lee@gmail.com>
2021-06-26 12:03:46 +1200

    add cypress

---
+++ components/Logo.cy.js
@@ -0,0 +1,11 @@
+import { mount } from '@cypress/vue'
+import Logo from './Logo.vue'
+
+describe('Logo', () => {
+  it('renders a logo', () => {
+    const msg = 'Hello Cypress Component Testing!'
+    mount(Logo)
+
+    cy.get('svg.NuxtLogo').should('exist')
+  })
+})
---
+++ cypress/plugins/index.js
@@ -0,0 +1,19 @@
+const { startDevServer } = require('@cypress/webpack-dev-server')
+// const webpackConfig = require('@vue/cli-service/webpack.config.js')
+const { getWebpackConfig } = require('nuxt')
+
+module.exports = (on, config) => {
+  on('dev-server:start', async (options) => {
+    let webpackConfig = await getWebpackConfig('client', {
+      for: 'dev',
+    })
+    delete webpackConfig.output
+    webpackConfig.plugins = webpackConfig.plugins.filter((a) => a.constructor.name !== 'HtmlWebpackPlugin')
+    return startDevServer({
+      options,
+      webpackConfig
+    })
+  })
+
+  return config
+}
