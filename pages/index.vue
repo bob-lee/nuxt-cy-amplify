640a03b7cfefff04ef22b0eec8388de86b8b8a49
bob-lee <bob.bumsuk.lee@gmail.com>
2021-07-02 16:59:57 +1200

    fetch response.ok to be false in case of 400 error

--- pages/index.vue
+++ pages/index.vue
@@ -49,58 +49,27 @@ export default {
         .then(response => response.json())
         .then(data => console.log(data));
     },
-    async postRevoke(id) { // https://stackoverflow.com/questions/54163952/async-await-in-fetch-how-to-handle-errors
+    async postRevoke(id) {
       const data = { client: { id }}
-      const response = await fetch('https://8xbpv0j4mb.execute-api.ap-southeast-2.amazonaws.com/v1/revoke-access', {
+      const response = await fetch('https://my-api-url', {
         method: 'POST',
-        // mode: 'no-cors',
         headers: {
-          // 'Accept': 'application/json',
           'Content-Type': 'application/json',
-          'x-api-key': '7mgOJolrFJ2YHAtqThda48B2XXhl3ngn1JEQxTM4'
+          'x-api-key': 'my-api-key'
         },
         body: JSON.stringify(data)
       })
 
-      return response.json()
+      if (response.ok) {
+        return response.json()
+      } else { // not ok
+        throw new Error(`Bad response ${response.status} from server: ${response.statusText}`)
+      }
     },
     search3() {
-      this.postRevoke(-124)
-        .then(data => console.log(data))
-        .catch((error) => {
-          console.error('Error:', error);
-        });
-      // fetch('https://8xbpv0j4mb.execute-api.ap-southeast-2.amazonaws.com/v1/revoke-access', {
-      //   method: 'POST',
-      //   // mode: 'no-cors',
-      //   headers: {
-      //     'Accept': 'application/json',
-      //     'Content-Type': 'application/json',
-      //     'x-api-key': '7mgOJolrFJ2YHAtqThda48B2XXhl3ngn1JEQxTM4'
-      //   },
-      //   body: JSON.stringify(data)
-      // })
-      //   .then(response => response.json())
-      //   .then(data => console.log('revoked', data))
-      //   .catch(error => {
-      //     console.error('fetch error', error)
-      //   })
-        // .then(response => response.json())
-        // .then(data => console.log(data));
-    },
-    search4() {
-      const data = { client: { id: -125 }}
-      Axios.post('https://8xbpv0j4mb.execute-api.ap-southeast-2.amazonaws.com/v1/revoke-access',
-        data,
-        {
-          headers: {
-            'x-api-key': '7mgOJolrFJ2YHAtqThda48B2XXhl3ngn1JEQxTM4'
-          }
-        }
-      )
-        .then(repsonse => console.log('response', response))
-        .catch(error => console.log(error.message ?? error));
-        // .catch(console.log);
+      this.postRevoke(1281928)
+        .then(console.log)
+        .catch(console.error)
     },
     async search() {
       const response = await fetch('https://restcountries.eu/rest/v2/name/' + this.term)
