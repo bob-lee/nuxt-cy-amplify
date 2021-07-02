832a69caeb619fd15cbaab3ccad9e17d3e5504f6
Bob Lee <bob@mytrucking.com>
2021-07-02 16:05:43 +1200

    how to handle 400 errors from fetch call

--- pages/index.vue
+++ pages/index.vue
@@ -22,7 +22,7 @@
         >
           GitHub
         </a>
-        <button data-cy="graphql" @click="search4">graphql</button>
+        <button data-cy="graphql" @click="search3">graphql</button>
         <input data-cy="term" placeholder="Country name" v-model="term" />
         <button data-cy="search" @click="search">Search</button>
         <span data-cy="countries">{{countries.length}}</span>
@@ -32,7 +32,7 @@
 </template>
 
 <script>
-import Axios from 'axios'
+import Axios from 'axios';
 
 const API_URL = 'https://my-graphql-server.com/graphql'
 
@@ -49,28 +49,48 @@ export default {
         .then(response => response.json())
         .then(data => console.log(data));
     },
-    search3() { // '7mgOJolrFJ2YHAtqThda48B2XXhl3ngn1JEQxTM4'
-      fetch('https://8xbpv0j4mb.execute-api.ap-southeast-2.amazonaws.com/v1', {
+    async postRevoke(id) { // https://stackoverflow.com/questions/54163952/async-await-in-fetch-how-to-handle-errors
+      const data = { client: { id }}
+      const response = await fetch('https://8xbpv0j4mb.execute-api.ap-southeast-2.amazonaws.com/v1/revoke-access', {
         method: 'POST',
-        mode: 'no-cors',
+        // mode: 'no-cors',
         headers: {
-          'Accept': 'application/json',
+          // 'Accept': 'application/json',
           'Content-Type': 'application/json',
           'x-api-key': '7mgOJolrFJ2YHAtqThda48B2XXhl3ngn1JEQxTM4'
         },
-        body: JSON.stringify({ 'client': { 'id': -123 }})
+        body: JSON.stringify(data)
       })
-        .then(response => response.json())
-        .then(data => console.log('revoked', data))
-        .catch(error => {
-          console.error('fetch error', error)
-        })
+
+      return response.json()
+    },
+    search3() {
+      this.postRevoke(-124)
+        .then(data => console.log(data))
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+      // fetch('https://8xbpv0j4mb.execute-api.ap-southeast-2.amazonaws.com/v1/revoke-access', {
+      //   method: 'POST',
+      //   // mode: 'no-cors',
+      //   headers: {
+      //     'Accept': 'application/json',
+      //     'Content-Type': 'application/json',
+      //     'x-api-key': '7mgOJolrFJ2YHAtqThda48B2XXhl3ngn1JEQxTM4'
+      //   },
+      //   body: JSON.stringify(data)
+      // })
+      //   .then(response => response.json())
+      //   .then(data => console.log('revoked', data))
+      //   .catch(error => {
+      //     console.error('fetch error', error)
+      //   })
         // .then(response => response.json())
         // .then(data => console.log(data));
     },
     search4() {
-      const data = { 'client': { 'id': -123 }}
-      Axios.post('https://8xbpv0j4mb.execute-api.ap-southeast-2.amazonaws.com/v1',
+      const data = { client: { id: -125 }}
+      Axios.post('https://8xbpv0j4mb.execute-api.ap-southeast-2.amazonaws.com/v1/revoke-access',
         data,
         {
           headers: {
@@ -79,8 +99,8 @@ export default {
         }
       )
         .then(repsonse => console.log('response', response))
-        .catch(error => console.log('error', error));
-
+        .catch(error => console.log(error.message ?? error));
+        // .catch(console.log);
     },
     async search() {
       const response = await fetch('https://restcountries.eu/rest/v2/name/' + this.term)
